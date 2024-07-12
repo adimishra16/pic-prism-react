c8e8497c720163738bed10cff1a9edfd38f945b4
adimishra16 <adimishra016@gmail.com>
2024-07-12 23:39:16 +0530

    Setup Redux on LoginPage using slice

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,20 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom' 
-import Navbar from './components/Navbar'
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import GsapTransition from "./components/GsapTransition";
+import { Provider } from "react-redux";
+import { store } from "../store/store";
 import Footer from './components/Footer'
-import GsapTransition from './components/GsapTransition'
+
 export default function App() {
   return (
     <>
-      <BrowserRouter>
-      <Navbar/>
-      <GsapTransition/>
-      <Footer/>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar />
+          <GsapTransition />
+          <Footer/>
+        </BrowserRouter>
+      </Provider>
     </>
-  )
+  );
 }
