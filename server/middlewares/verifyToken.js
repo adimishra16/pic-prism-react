a8ac2e1c5b3f2f493610d2b3565146b4fd7bb340
adimishra16 <adimishra016@gmail.com>
2024-07-23 15:44:48 +0530

    Image Upload Fun

--- server/middlewares/verifyToken.js
+++ server/middlewares/verifyToken.js
@@ -1,23 +1,34 @@
-const jwt=require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-const verifyToken=async(req,res,next)=>{
-    
-    const authHeader=req.header('Authorization')
-    const token=authHeader && authHeader.split(" ")[1];
+// middleware has req,res,next
+const verifyToken = async (req, res, next) => {
+  const authHeader = req.header("Authorization");
+  const token = authHeader && authHeader.split(" ")[1];
 
-    if(!token) return res.status(401).json({success:false,message:"Unauthorized"});
-    try {
-        jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
-            if(err) return res.status(403).json({success:false,message:"Forbidden"});
+  if (!token)
+    return res.status(401).json({ success: false, message: "Unauthorized" });
 
-            req.id=user.idl;
-            req.author=user.author;
-            req.accountType=user.accountType;
-            next();
+  try {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if (err)
+        return res.status(403).json({ success: false, message: "Forbidden" });
 
-        })
+      req.id = user.id;
+      req.author = user.author;
+      req.accountType = user.accountType;
 
-    } catch (error) {
-        return res.status(500).json({success:false,message:"Internal Server Error"});
-    }
-}
+      next();
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+module.exports = { verifyToken };
+
+// Bearer ej72852dadsgasggdafaeer23
+// we have to split -> " "
+// ["Bearer", "ej72852dadsgasggdafaeer23"]
+// [1]
